55bc90bcc9750e6a73378748dec71a8cc7d401d3
Samuel Trujillo Rodriguez <77857106+samutrujillo@users.noreply.github.com>
2025-03-25 16:41:29 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -54,16 +54,16 @@ const io = new Server(server, {
 
 // Datos de usuario (en una aplicación real, esto estaría en una base de datos)
 const users = [
-    { id: '1', username: 'jugador1', password: 'clave1', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
-    { id: '2', username: 'jugador2', password: 'clave2', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
-    { id: '3', username: 'jugador3', password: 'clave3', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
-    { id: '4', username: 'jugador4', password: 'clave4', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
-    { id: '5', username: 'jugador5', password: 'clave5', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
-    { id: '6', username: 'jugador6', password: 'clave6', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
-    { id: '7', username: 'jugador7', password: 'clave7', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
-    { id: '8', username: 'jugador8', password: 'clave8', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
-    { id: '9', username: 'jugador9', password: 'clave9', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
-    { id: '10', username: 'jugador10', password: 'clave10', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '1', username: 'Orion', password: '3498700', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '2', username: 'Andy', password: '2587411', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '3', username: 'Casio', password: '9632541', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '4', username: 'Pega', password: '7412589', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '5', username: 'Percy', password: '8523697', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '6', username: 'Nova', password: '1234567', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '7', username: 'Leo', password: '7654321', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '8', username: 'Ara', password: '1122334', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '9', username: 'Hydra', password: 'serpiente321', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
+    { id: '10', username: 'Lyra', password: 'arpa987', score: 60000, prevScore: 60000, isAdmin: false, isBlocked: false, isLockedDueToScore: false },
     { id: 'admin', username: 'admin', password: 'admin1998', score: 60000, prevScore: 60000, isAdmin: true, isBlocked: false, isLockedDueToScore: false }
 ];
 
